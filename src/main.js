e9498f4d8c610e393d02fb0f80dc52da12caa06d
BBarbs <Blake.Barber@hhs.texas.gov>
2025-01-16 08:58:55 -0600

    Package installs

--- src/main.js
+++ src/main.js
@@ -5,10 +5,30 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueApexCharts from "vue3-apexcharts";
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+    },
+    theme: {
+        defaultTheme: 'light'
+    }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+app.use(VueApexCharts)
 
 app.mount('#app')
