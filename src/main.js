5cb50ad8035e372e5886755a18cfa805c3e28857
BBarbs <barber.blake.d@gmail.com>
2025-01-17 14:44:02 -0600

    Cleanup

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 import VueApexCharts from "vue3-apexcharts";
 
 // Vuetify
@@ -27,7 +26,6 @@ const vuetify = createVuetify({
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 app.use(vuetify)
 app.use(VueApexCharts)
 
